1a729825758b1a50f860e2ba879b76a922b81922
System Administrator <root@Tareqs-MacBook-Pro.local>
2023-04-26 00:02:15 +0300

    animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,298 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, CssBaseline } from "@mui/material";
+import React from "react";
+import {
+  container,
+  buttonContainer,
+  ray1,
+  ray2,
+  ray3,
+  cloud,
+  cloud2,
+  cloud3,
+  cloud4,
+  cloud5,
+  cloud6,
+  cloud7,
+  cloud8,
+  cloud9,
+  cloud10,
+} from "./styles";
+import { Star } from "@mui/icons-material";
+const App = () => {
+  const [click, setClick] = React.useState(true);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={container}>
+      <CssBaseline />
+      <Box
+        sx={{
+          ...buttonContainer,
+          backgroundColor: click ? "#2e83bf" : "#333",
+          transition: "1s",
+        }}
+      >
+        <Box
+          sx={{
+            ...ray1,
+            // display: click ? "block" : "none",
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            left: click ? -100 : 200,
+            transition: "0.75s",
+            zIndex: 1,
+          }}
+        ></Box>
+        <Box
+          sx={{
+            ...ray2,
+            // display: click ? "block" : "none",
+            backgroundColor: "rgba(255, 255, 255, 0.05)",
+            left: click ? -50 : 150,
+            transition: "0.75s",
+            zIndex: 2,
+          }}
+        ></Box>
+        <Box
+          sx={{
+            ...ray3,
+            // display: click ? "block" : "none",
+            backgroundColor: "rgba(255, 255, 255, 0.025)",
+            left: click ? 0 : 100,
+            transition: "0.75s",
+            zIndex: 3,
+          }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{
+            ...cloud,
+            bottom: click ? 90 : -400,
+            right: -50,
+            transition: "1s",
+          }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{
+            ...cloud2,
+            bottom: click ? 5 : -400,
+            right: -5,
+            transition: "1s",
+          }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{ ...cloud3, bottom: click ? -40 : -400, transition: "1s" }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{ ...cloud4, bottom: click ? -45 : -400, transition: "1s" }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{ ...cloud5, bottom: click ? -80 : -400, transition: "1s" }}
+        ></Box>
+        {/* Transparent Clouds */}
+        <Box
+          id="cloud"
+          sx={{ ...cloud6, bottom: click ? 110 : -400, transition: "1s" }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{ ...cloud7, bottom: click ? 60 : -400, transition: "1s" }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{ ...cloud8, bottom: click ? 15 : -400, transition: "1s" }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{ ...cloud9, bottom: click ? -20 : -400, transition: "1s" }}
+        ></Box>
+        <Box
+          id="cloud"
+          sx={{ ...cloud10, bottom: click ? -65 : -400, transition: "1s" }}
+        ></Box>
+        <Box
+          id="Sun"
+          onClick={() => setClick((prevValue) => !prevValue)}
+          sx={{
+            backgroundColor: click ? "#fbc72d" : "#cccfd9",
+            height: "190px",
+            width: "190px",
+            position: "absolute",
+            zIndex: 10,
+            left: click === true ? "10%" : "55%",
+            top: "12.5%",
+            borderRadius: "50%",
+            boxShadow: "inset 0px -5px 10px 0px rgba(126, 126, 126, 0.78)",
+            "&:hover": {
+              cursor: "pointer",
+              transform: "scale(1.01)",
+            },
+            transform: !click && "rotate(-15deg)",
+            transition: "0.75s",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box
+            id="hole"
+            sx={{
+              height: "60px",
+              width: "60px",
+              backgroundColor: click ? "transparent" : "#9fa7bc",
+              borderRadius: "50%",
+              position: "absolute",
+              left: 30,
+              bottom: 50,
+              boxShadow: !click && "inset 0px 1px 1px 0px",
+              transition: "0.8s",
+            }}
+          ></Box>
+          <Box
+            id="hole"
+            sx={{
+              height: "35px",
+              width: "35px",
+              backgroundColor: click ? "transparent" : "#9fa7bc",
+              borderRadius: "50%",
+              position: "absolute",
+              left: 75,
+              top: 35,
+              boxShadow: !click && "inset 0px 1px 1px 0px",
+              transition: "0.8s",
+            }}
+          ></Box>
+          <Box
+            id="hole"
+            sx={{
+              height: "35px",
+              width: "35px",
+              backgroundColor: click ? "transparent" : "#9fa7bc",
+              borderRadius: "50%",
+              position: "absolute",
+              left: 115,
+              bottom: 35,
+              boxShadow: !click && "inset 0px 1px 1px 0px",
+              transition: "0.8s",
+            }}
+          ></Box>
+          {/* End of Sun Container */}
+        </Box>
+        <Star
+          sx={{
+            fontSize: "2rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 100,
+            top: click ? -100 : 50,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "2.5rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 225,
+            top: click ? -100 : 50,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "2.2rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 175,
+            top: click ? -100 : 150,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        {/* Small Stars right Side*/}
+        <Star
+          sx={{
+            fontSize: "0.9rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 150,
+            top: click ? -100 : 115,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "0.9rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 175,
+            top: click ? -100 : 35,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "0.9rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 250,
+            top: click ? -100 : 100,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "0.9rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 225,
+            top: click ? -100 : 150,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "0.9rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 100,
+            top: click ? -100 : 180,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "0.9rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 75,
+            top: click ? -100 : 40,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        <Star
+          sx={{
+            fontSize: "0.9rem",
+            fill: click ? "transparent" : "white",
+            position: "absolute",
+            left: 40,
+            top: click ? -100 : 140,
+            zIndex: 5,
+            transition: "1s",
+          }}
+        />
+        {/* End Of Small Stars Right Side */}
+        {/* End Of Button Containe */}
+      </Box>
+    </Box>
   );
-}
+};
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,156 @@
+export const container = {
+  height: "100vh",
+  width: "100vw",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "lightGray",
+};
+
+export const buttonContainer = {
+  width: "550px",
+  height: "240px",
+  borderRadius: 50,
+
+  position: "relative",
+  overflow: "hidden",
+  border: "none",
+  boxShadow:
+    "0px -5px 5px 0px rgba(0, 0, 0, 0.3),inset 0px 15px 10px 0px rgba(0, 0, 0, 0.3),inset 0px -5px 5px 0px rgba(255, 255, 255, 0.8), 0px 5px 5px 0px rgba(255, 255, 255, 0.5)",
+};
+
+export const ray1 = {
+  height: "150%",
+  width: "75%",
+  borderRadius: "50%",
+  position: "absolute",
+
+  top: -50,
+};
+
+export const ray2 = {
+  height: "150%",
+  width: "75%",
+  borderRadius: "50%",
+  position: "absolute",
+
+  top: -50,
+};
+export const ray3 = {
+  height: "150%",
+  width: "75%",
+  borderRadius: "50%",
+  position: "absolute",
+  top: -50,
+};
+
+export const cloud = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "white",
+  position: "absolute",
+
+  zIndex: 1,
+};
+export const cloud2 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "white",
+  position: "absolute",
+
+  border: "none",
+  zIndex: 1,
+};
+export const cloud3 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "white",
+  position: "absolute",
+
+  right: 50,
+  border: "none",
+  zIndex: 1,
+};
+export const cloud4 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "white",
+  position: "absolute",
+
+  right: 150,
+  border: "none",
+  zIndex: 1,
+};
+export const cloud5 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "white",
+  position: "absolute",
+
+  right: 260,
+  border: "none",
+  zIndex: 1,
+};
+
+// Transparent Clouds
+
+export const cloud6 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "rgb(178,208,234)",
+  position: "absolute",
+
+  right: -12.5,
+  border: "none",
+  zIndex: 0,
+};
+export const cloud7 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "rgb(178,208,234)",
+  position: "absolute",
+
+  right: 25,
+  border: "none",
+  zIndex: 0,
+};
+export const cloud8 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "rgb(178,208,234)",
+  position: "absolute",
+
+  right: 125,
+  border: "none",
+  zIndex: 0,
+};
+export const cloud9 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "rgb(178,208,234)",
+  position: "absolute",
+
+  right: 200,
+  border: "none",
+  zIndex: 0,
+};
+export const cloud10 = {
+  height: "50%",
+  width: "25%",
+  borderRadius: "50%",
+  backgroundColor: "rgb(178,208,234)",
+  position: "absolute",
+
+  right: 275,
+  border: "none",
+  zIndex: 0,
+};
